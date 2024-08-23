b30bc528874841ac7cecefe85e6fd8e2c61e2b11
Benzazoua <billalben252@gmail.com>
2024-08-23 12:17:46 +0100

    Frontend Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape";
+import RegisterPage from "./RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+const App = () => {
+  return (
+    <div className="relative flex min-h-screen items-center justify-center overflow-hidden bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900">
+      <FloatingShape
+        color="bg-green-500"
+        size="w-64 h-64"
+        top="-5%"
+        left="10%"
+        delay={0}
+      />
+      <FloatingShape
+        color="bg-emerald-500"
+        size="w-48 h-48"
+        top="70%"
+        left="80%"
+        delay={5}
+      />
+      <FloatingShape
+        color="bg-lime-500"
+        size="w-32 h-32"
+        top="40%"
+        left="-10%"
+        delay={2}
+      />
+
+      <Routes>
+        <Route path="/" element={<h1>dashboard page</h1>} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<h1>verify email page</h1>} />
+        <Route
+          path="/forgot-password"
+          element={<h1>forgot password page</h1>}
+        />
+        <Route
+          path="/reset-password/:token"
+          element={<h1>reset password page</h1>}
+        />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/FloatingShape.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+
+type TProps = {
+  color: string;
+  size: string;
+  top: string;
+  left: string;
+  delay: number;
+};
+
+const FloatingShape = ({ color, size, top, left, delay }: TProps) => {
+  return (
+    <motion.div
+      className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
+      style={{ top, left }}
+      animate={{
+        y: ["0%", "100%", "0%"],
+        x: ["0%", "100%", "0%"],
+        rotate: [0, 360],
+      }}
+      transition={{
+        duration: 20,
+        ease: "linear",
+        repeat: Infinity,
+        delay,
+      }}
+      aria-hidden="true"
+    />
+  );
+};
+export default FloatingShape;
