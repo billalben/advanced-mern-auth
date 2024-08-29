a94513a9f6be27e9b16ca102304e237e5112cf2e
Benzazoua <billalben252@gmail.com>
2024-08-29 21:01:15 +0100

    Login Page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,89 @@
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+import { FormEvent, useState } from "react";
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
+import { useAuthStore } from "../store/authStore";
 
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, isLoading, error } = useAuthStore();
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await login(email, password);
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full max-w-md overflow-hidden bg-gray-800 bg-opacity-50 shadow-xl rounded-2xl backdrop-blur-xl backdrop-filter"
+    >
+      <div className="p-8">
+        <h2 className="mb-6 text-3xl font-bold text-center text-transparent bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text">
+          Welcome Back
+        </h2>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            aria-invalid={!!error}
+          />
+
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            aria-invalid={!!error}
+          />
+
+          <div className="flex items-center mb-6">
+            <Link
+              to="/forgot-password"
+              className="text-sm text-green-400 hover:underline"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          {error && <p className="mb-2 font-semibold text-red-500">{error}</p>}
+
+          <motion.button
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            className="w-full px-4 py-3 font-bold text-white transition duration-200 rounded-lg shadow-lg bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <Loader className="w-6 h-6 mx-auto animate-spin" />
+            ) : (
+              "Login"
+            )}
+          </motion.button>
+        </form>
+      </div>
+
+      <div className="flex justify-center px-8 py-4 bg-gray-900 bg-opacity-50">
+        <p className="text-sm text-gray-400">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-green-400 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
+};
 export default LoginPage;
