5e1f8913d3123b670ad0d50771933098fbe15f89
Benzazoua <billalben252@gmail.com>
2024-09-22 18:13:03 +0100

    For production

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import path from "path";
+import path from "path";
 
 import { connectDB, disconnectDB } from "./config/connectDB.js";
 
@@ -14,7 +14,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5011;
-// const __dirname = path.resolve();
+
+const __dirname = path.resolve(); // To get the root path not the current path
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
@@ -22,6 +23,14 @@ app.use(express.json()); // To parse JSON bodies
 app.use(cookieParser()); // To parse cookies in the request headers
 
 app.use("/api/auth", authRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 const server = app.listen(PORT, async () => {
   await connectDB(process.env.MONGODB_URI);
