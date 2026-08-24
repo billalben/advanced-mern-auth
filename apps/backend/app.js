a2b7507fab6e9f73a74db924744c3066d8b46df9
Benzazoua <billalben252@gmail.com>
2026-08-24 20:00:31 +0100

    feat(root): change folder structure

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -25,10 +25,10 @@ app.use(cookieParser()); // To parse cookies in the request headers
 app.use("/api/auth", authRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"));
   });
 }
 
