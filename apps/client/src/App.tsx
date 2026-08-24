01d0e93a28429abfe4fa5a9b06d1064ee802b390
Benzazoua <billalben252@gmail.com>
2026-08-24 20:26:30 +0100

    feat(root): create new client project
    - create a separate project vite

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,109 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import type * as React from "react";
+import { Toaster } from "react-hot-toast";
+import FloatingShapes from "./components/FloatingShapes";
+import { useAuthStore } from "./store/authStore";
+
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const VerifyEmailPage = lazy(() => import("./pages/EmailVerificationPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const ForgotPasswordPage = lazy(() => import("./pages/ForgotPasswordPage"));
+const ResetPasswordPage = lazy(() => import("./pages/ResetPasswordPage"));
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (user?.isVerified === false) {
+    return <Navigate to="/verify-email" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+const RedirectAuthenticatedUser = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isAuthenticated, user } = useAuthStore();
+
+  if (isAuthenticated && user?.isVerified) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+const App = () => {
+  return (
+    <div className="relative flex items-center justify-center min-h-screen overflow-hidden bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900">
+      <FloatingShapes />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* Public Routes */}
+          <Route
+            path="/register"
+            element={
+              <RedirectAuthenticatedUser>
+                <RegisterPage />
+              </RedirectAuthenticatedUser>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RedirectAuthenticatedUser>
+                <LoginPage />
+              </RedirectAuthenticatedUser>
+            }
+          />
+          <Route
+            path="/verify-email"
+            element={
+              <RedirectAuthenticatedUser>
+                <VerifyEmailPage />
+              </RedirectAuthenticatedUser>
+            }
+          />
+
+          <Route
+            path="/forgot-password"
+            element={
+              <RedirectAuthenticatedUser>
+                <ForgotPasswordPage />
+              </RedirectAuthenticatedUser>
+            }
+          />
+
+          <Route
+            path="/reset-password/:token"
+            element={
+              <RedirectAuthenticatedUser>
+                <ResetPasswordPage />
+              </RedirectAuthenticatedUser>
+            }
+          />
+
+          {/* Protected Route */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
